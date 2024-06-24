4d0992286835a57dc7f6fc98314f1fad4697e569
Lucas Walter <lucas.walter@ucsal.edu.br>
2024-06-24 13:47:54 -0300

    finish screenmatch-v3

--- src/main/java/br/com/alura/screenmatch/service/ApiConsume.java
+++ src/main/java/br/com/alura/screenmatch/service/ApiConsume.java
@@ -18,12 +18,9 @@ public class ApiConsume {
         try {
             response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
-        String json = response.body();
-        return json;
+        return response.body();
     }
 }
